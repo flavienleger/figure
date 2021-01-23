a46421f4f350b5318d02fecd2ef5734dfb98e320
Flavien Léger <flavienleger@users.noreply.github.com>
2021-01-23 18:52:57 +0100

    Use imsave instead of imshow

--- visualization.py
+++ visualization.py
@@ -1,6 +1,4 @@
-#!/usr/local/bin/python3
 
-#%%
 import numpy as np 
 import matplotlib.pyplot as plt
 import os.path
@@ -20,7 +18,10 @@ def open_and_reshape(filename, n1, n2):
 
 def view_heatmap(rho, cmap_name='inferno'):
 	
-	fig, ax = plt.subplots(figsize=(4,4))
+	# fig, ax = plt.subplots(figsize=(4,4))
+	fig, ax = plt.subplots()
+	# n1, n2 = rho.shape 
+	# fig, ax = plt.subplots(figsize=(1,n2/n1))
 	ax.set_axis_off()
 	fig.subplots_adjust(bottom=0, top=1, right=1, left=0)
 
@@ -30,23 +31,37 @@ def view_heatmap(rho, cmap_name='inferno'):
 	plt.show()
 
 
-def create_heatmap(filename, rho, cmap_name='inferno'):
-	# Create 4in x 4in figure (4in in case we also want to show() it -- better than 1in)
-	fig, ax = plt.subplots(figsize=(4,4))
-	ax.set_axis_off()
-	fig.subplots_adjust(bottom=0, top=1, right=1, left=0)
-	n1, n2 = rho.shape
-	dpi = n1 / 4.0
 
-	cmap = plt.get_cmap(cmap_name)
-	ax.imshow(rho, cmap=cmap, interpolation='bicubic')
+# Don't need this, use built-in plt.imsave() instead
+# def create_heatmap(filename, rho, cmap_name='inferno'):
+# 	# Create 4in x 4in figure (4in in case we also want to show() it -- better than 1in)
+# 	# fig, ax = plt.subplots(figsize=(4,4))
+# 	fig, ax = plt.subplots()
+# 	ax.set_axis_off()
+# 	fig.subplots_adjust(bottom=0, top=1, right=1, left=0)
+# 	n1, n2 = rho.shape
+# 	dpi = n1 / 4.0
+
+# 	cmap = plt.get_cmap(cmap_name)
+# 	ax.imshow(rho, cmap=cmap, interpolation='bicubic')
 	
-	fig.savefig(filename, dpi=dpi)
+# 	fig.savefig(filename, dpi=dpi)
 
 
 
 def parse_filename(filename):
-	"   '/folder/rho-001.data' returns ('/folder', 'rho-', 1, 3, '.data')  "
+	"""Parse filename of a frame. Example:
+	
+	Input:
+	filename = '/fol/der/rho-001.data'
+
+	Output:
+	dirname 	= '/fol/der'
+	prefix 	 	= 'rho-'
+	index		= 1 		(integer)
+	index_width = 3			(integer)
+	extension	= '.data'	(can be empty)	   
+	"""
 	tmp, extension = os.path.splitext(filename)
 	dirname, basename = os.path.split(tmp)
 
@@ -58,55 +73,30 @@ def parse_filename(filename):
 
 
 
-def create_heatmap_movie(initial_filename, num_frames, n1, n2, cmap_name='inferno', interp_name='bicubic', movie_name='movie.mp4'):
-	
-	# Parse filename. If filename = '/fol/der/rho-001.data' then
-	# 	dirname 	= '/fol/der'
-	# 	prefix 	 	= 'rho-'
-	# 	index		= 1 		(integer)
-	# 	index_width = 3			(integer)
-	# 	extension	= '.data'	(can be empty)
-
+def create_heatmap_movie(initial_filename, num_frames, n1, n2, cmap_name='inferno', movie_name='movie.mp4'):
+	"""Create heatmap image files from binary files and assemble them into a movie.
+	"""
 	dirname, prefix, index, index_width, extension = parse_filename(initial_filename)
 
 	rho = open_and_reshape(initial_filename, n1, n2)
 
-	# Create 4in x 4in figure (4in in case we also want to show() it -- better than 1in)
-	# fig, ax = plt.subplots(figsize=(4,4))
-	fig, ax = plt.subplots(figsize=(1,1))
-	ax.set_axis_off()
-	fig.subplots_adjust(bottom=0, top=1, right=1, left=0)
-	# dpi = n1 / 4.0
-	dpi = n1
-
-	# Colomaps I like: plasma, inferno, cividis, binary
-	cmap = plt.get_cmap(cmap_name)		
-	im = ax.imshow(rho, cmap=cmap, interpolation=interp_name)
-
 	for k in range(num_frames+1):
-		# print("\rProcessing frame %d/%d..." % (k, num_frames), end='')
 		print(f'\rCreating frame {k}/{num_frames}...', end='')
 		
 		basename = os.path.join(dirname, prefix + f'{index+k:0{index_width}}')
 
 		rho = open_and_reshape(basename + extension, n1, n2)
-		
-		im.set_data(rho)			
-
-		fig.savefig(basename+'.png', dpi=dpi)
+	
+		plt.imsave(basename+'.png', rho, cmap=cmap_name)
 
 	print("done")
 
 	ffmpeg_cmd = f'ffmpeg -framerate 30 -i {dirname}/{prefix}%0{index_width}d.png -f mp4 -an -c:v libx264 -preset veryslow -crf 18 -pix_fmt yuv420p -r 30 {movie_name}'
-	# print(ffmpeg_cmd)
 	os.system(ffmpeg_cmd)
 
 
 
 
-
 # %%
-# rho = open_and_reshape('/Users/fun/Dropbox/work/recherche/ot/mattwonjunflavien/gf/wgfBFMcodes/data/rho-0000.dat', 512, 512)
-# view_heatmap(rho)
-# create_heatmap('/Users/fun/Dropbox/work/recherche/ot/mattwonjunflavien/gf/wgfBFMcodes/hm.png', rho)
-
+create_heatmap_movie('/Users/fun/Dropbox/work/recherche/ot/mattwonjunflavien/gf/wgfBFMcodes/data/rho-0000.dat', 80, 512, 512)
+# %%
